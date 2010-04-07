b9577673ea1a1bfec74030cfb8933ae2cbbd34aa
ppershing <ppershing@3152dc9c-92df-51e6-821f-c0386a2f891b>
2010-04-07 21:58:12 +0000

    Upgradnuty testovac, momentalne zvlada aj submitovat.

--- testovac/test.cpp
+++ testovac/test.cpp
@@ -9,19 +9,26 @@ int main() {
     vector<string> compile_output;
     vector<string> input;
 
+    vector<string> desc;
+    desc = Testovac::get_task_description("sucet");
+    printf("zadanie ulohy:\n");
+    for (int i=0; i<(int)desc.size(); i++) {
+        printf("%s\n", desc[i].c_str());
+    }
+    printf("\n");
+
     compile_settings.compiler = CompileSettings::COMPILER_CPP;
     compile_settings.compile_with_warnings = 0;
 
-    test_settings.memory_limit = 100000;
+    test_settings.memory_limit = 1000;
     test_settings.time_limit = 1000;
 
     program.push_back("#include <stdio.h>");
     program.push_back("int main() {");
     program.push_back(" int nepouzivana;");
-    program.push_back(" int cislo;");
-    program.push_back(" printf(\"Zadaj cislo:\");");
-    program.push_back(" scanf(\"%d\", &cislo);");
-    program.push_back(" printf(\"Hello world %d\\n\",cislo);");
+    program.push_back(" int a,b;");
+    program.push_back(" scanf(\"%d %d\", &a,&b);");
+    program.push_back(" printf(\"%d\\n\", a+b);");
     program.push_back("}");
 /*    program.push_back("var nepouzivana,cislo: integer;");
     program.push_back("begin");    
@@ -30,13 +37,18 @@ int main() {
     program.push_back("writeln('Hello world!', cislo);");
     program.push_back("end.");*/
 
-    input.push_back("5");
 
-    int retval = Testovac::test_on_input(program, input,
-            compile_settings,test_settings, &compile_output, &output);
+    int retval = Testovac::submit_solution("sucet", program,
+            compile_settings,test_settings,
+            &compile_output, &output);
 
-    printf("compiling returned value %d\n", retval);
-    printf("errors encountered:\n");
+    printf("Testing returned value %d\n", retval);
+    printf("Data from compilation:\n");
+    for (int i=0; i<(int)compile_output.size(); i++) {
+        printf("%s\n", compile_output[i].c_str());
+    }
+    printf("\n");
+    printf("Data from log:\n");
     for (int i=0; i<(int)output.size(); i++) {
         printf("%s\n", output[i].c_str());
     }
