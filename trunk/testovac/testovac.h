34b2a820f0cbca0106920ffd3b28de673d53de0c
ppershing <ppershing@3152dc9c-92df-51e6-821f-c0386a2f891b>
2010-04-07 21:13:23 +0000

    Dalsia verzia testovaca, momentalne funguje compile() a test_on_input()

--- trunk/testovac/testovac.h
+++ trunk/testovac/testovac.h
@@ -10,13 +10,44 @@ public:
     int compile_with_warnings;
 };
 
+class TestSettings {
+public:
+    int memory_limit; // in kB
+    int time_limit; // in msec
+};
+
 class Testovac {
 private:
     static void clean_sandbox();
+    static void copy_wrapper();
+    static void copy_test_data(const char* taskname);
     static void write_to_file(const char * filename, vector<string>& lines);
     static void read_from_file(const char * filename, vector<string> *lines);
+    static int initialized;
+    static string testdir;
+    static string get_sandbox_dir();
+    static string get_wrapper_dir();
+    static string get_program_bin();
+    static string get_program_source(string ext);
+    static string inttostr(int i);
 public:
+    static void initialize(const char* testdir);
+
     static int compile(vector<string>& program,
             CompileSettings& settings,
             vector<string> *output);
+
+    static int test_on_input(vector<string>& program,
+            vector<string>& input,
+            CompileSettings& compile_settings,
+            TestSettings& test_setting,
+            vector<string> *compile_output,
+            vector<string> *test_output);
+
+    static int full_test(const char* taskname,
+            vector<string>& program,
+            CompileSettings& compile_settings,
+            TestSettings& test_settings,
+            vector<string> *compile_output,
+            vector<string> *test_output);
 };
