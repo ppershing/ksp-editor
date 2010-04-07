24c9f50ab8ae9896002146f8936a47baf59fa0a6
ppershing <ppershing@3152dc9c-92df-51e6-821f-c0386a2f891b>
2010-04-07 21:13:23 +0000

    Dalsia verzia testovaca, momentalne funguje compile() a test_on_input()

--- testovac/test.cpp
+++ testovac/test.cpp
@@ -1,28 +1,43 @@
 #include "testovac.cpp"
 
 int main() {
-    CompileSettings settings;
+    Testovac::initialize(".");
+    CompileSettings compile_settings;
+    TestSettings test_settings;
     vector<string> program;
     vector<string> output;
+    vector<string> compile_output;
+    vector<string> input;
 
-    settings.compiler = CompileSettings::COMPILER_PAS;
-    settings.compile_with_warnings = 0;
+    compile_settings.compiler = CompileSettings::COMPILER_CPP;
+    compile_settings.compile_with_warnings = 0;
 
-/*    program.push_back("#include <stdio.h>");
+    test_settings.memory_limit = 100000;
+    test_settings.time_limit = 1000;
+
+    program.push_back("#include <stdio.h>");
     program.push_back("int main() {");
     program.push_back(" int nepouzivana;");
-    program.push_back(" printf(\"Hello world\\n\");");
-    program.push_back("}");*/
-    program.push_back("var nepouzivana: integer;");
+    program.push_back(" int cislo;");
+    program.push_back(" printf(\"Zadaj cislo:\");");
+    program.push_back(" scanf(\"%d\", &cislo);");
+    program.push_back(" printf(\"Hello world %d\\n\",cislo);");
+    program.push_back("}");
+/*    program.push_back("var nepouzivana,cislo: integer;");
     program.push_back("begin");    
-    program.push_back("writeln('Hello world!');");
-    program.push_back("end.");
+    program.push_back("writeln('zadaj cislo:');");
+    program.push_back("readln(cislo);");
+    program.push_back("writeln('Hello world!', cislo);");
+    program.push_back("end.");*/
 
-    int retval = Testovac::compile(program, settings, &output);
+    input.push_back("5");
+
+    int retval = Testovac::test_on_input(program, input,
+            compile_settings,test_settings, &compile_output, &output);
 
     printf("compiling returned value %d\n", retval);
     printf("errors encountered:\n");
-    for (int i=0; i<output.size(); i++) {
+    for (int i=0; i<(int)output.size(); i++) {
         printf("%s\n", output[i].c_str());
     }
 }
--- testovac/testovac.cpp
+++ testovac/testovac.cpp
@@ -3,10 +3,47 @@
 #include <fstream>
 #include <cassert>
 
-void Testovac::clean_sandbox() {
-    system("rm -rf sandbox/*");
+string Testovac::testdir;
+int Testovac::initialized;
+
+void Testovac::initialize(const char* testdir){
+    Testovac::testdir = testdir;
+    Testovac::initialized = 1;
 }
 
+string Testovac::get_sandbox_dir(){
+    return testdir+"/sandbox";
+}
+
+string Testovac::get_wrapper_dir(){
+    return testdir;
+}
+
+string Testovac::get_program_bin(){
+    return get_sandbox_dir()+"/program";
+}
+
+string Testovac::get_program_source(string ext){
+    return get_program_bin()+"."+ext;
+}
+
+void Testovac::clean_sandbox() {
+    assert(initialized);
+    string s="rm -rf \""+get_sandbox_dir()+"/*\"";
+    system(s.c_str());
+}
+
+void Testovac::copy_wrapper() {
+    assert(initialized);
+    string wrapper = get_wrapper_dir()+"/wrapper";
+    string sandbox = get_sandbox_dir()+"/wrapper";
+    string s="cp \""+wrapper+"\" \""+sandbox+"\"";
+    system(s.c_str());
+}
+
+void Testovac::copy_test_data(const char* taskname) {
+    system((string("cp ") + taskname + "/* sandbox/").c_str());
+}
 
 int Testovac::compile(vector<string>& program,
             CompileSettings& settings,
@@ -15,19 +52,21 @@ int Testovac::compile(vector<string>& program,
     string compileLine;
     int retval;
 
+    assert(initialized);
     assert (settings.compiler == CompileSettings::COMPILER_CPP ||
             settings.compiler == CompileSettings::COMPILER_PAS);
 
     if (settings.compiler == CompileSettings::COMPILER_CPP) {
-        programName = "sandbox/program.cpp";
+        programName = get_program_source("cpp");
         compileLine = "g++ " + programName +
-                      " -o sandbox/program";
+                      " -static -static-libc" + 
+                      " -o " + get_program_bin();
         if (settings.compile_with_warnings) {
             compileLine+=" -W -Wall";
         }
     } else 
     if (settings.compiler == CompileSettings::COMPILER_PAS) {
-        programName = "sandbox/program.pas";
+        programName = get_program_source("pas");
         compileLine = "fpc " + programName;
         if (settings.compile_with_warnings) {
             compileLine+=" -veiwn";
@@ -39,19 +78,20 @@ int Testovac::compile(vector<string>& program,
     Testovac::clean_sandbox();
     write_to_file(programName.c_str(), program);
 
-    retval = system((compileLine + " &>sandbox/compile_err").c_str());
+    string error_file=get_sandbox_dir()+"/compile_err";
+    string s=compileLine + " &>"+error_file;
+    retval = system(s.c_str());
 
-    read_from_file("sandbox/compile_err", output);
+    read_from_file(error_file.c_str(), output);
     return retval;
 }
 
 void Testovac::write_to_file(const char * filename, vector<string>& lines) {
     ofstream s(filename);
-    for (int i=0; i<lines.size(); i++) {
+    for (int i=0; i<(int) lines.size(); i++) {
         s << lines[i] << endl;
     }
     s.close();
-    assert(s.fail() == 0);
 }
 
 void Testovac::read_from_file(const char * filename, vector<string> *lines) {
@@ -65,3 +105,53 @@ void Testovac::read_from_file(const char * filename, vector<string> *lines) {
     }
 }
 
+string Testovac::inttostr(int i){
+    char tmp[100];
+    sprintf(tmp,"%d",i);
+    return string(tmp);
+}
+
+int Testovac::test_on_input(vector<string> &program,
+        vector<string>& input,
+        CompileSettings& compile_settings,
+        TestSettings& test_settings,
+        vector<string> *compile_output,
+        vector<string> *test_output) {
+    int compile_ret;
+    compile_ret = Testovac::compile(program, compile_settings, compile_output);
+    if (compile_ret) return compile_ret;
+    Testovac::copy_wrapper();
+    string test_in = get_sandbox_dir()+"/test.in";
+    write_to_file(test_in.c_str(), input);
+    string test_out = get_sandbox_dir()+"/test.out";
+
+    string testcmd = "cd "+get_sandbox_dir()+" && ./wrapper -a0 -ff";
+    testcmd += " -m"+inttostr(test_settings.memory_limit);
+    testcmd += " -t"+inttostr(test_settings.time_limit);
+    testcmd += " -itest.in -otest.out";
+    testcmd += " ./program &>test.log";
+    int retval;
+    retval = system(testcmd.c_str());
+    read_from_file(test_out.c_str(), test_output);
+    return retval;
+
+}
+
+int Testovac::full_test(const char* taskname,
+        vector<string> &program,
+        CompileSettings& compile_settings,
+        TestSettings& test_settings,
+        vector<string> *compile_output,
+        vector<string> *test_output) {
+    int compile_ret;
+    compile_ret = Testovac::compile(program, compile_settings, compile_output);
+    if (compile_ret) return compile_ret;
+    Testovac::copy_wrapper();
+    Testovac::copy_test_data(taskname);
+
+    int retval;
+    retval = system("cd sandbox && ./testuj.sh 1>test_out 2>test_err");
+    read_from_file("sandbox/test_out", test_output);
+    return retval;
+
+}
