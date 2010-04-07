2296f803a515d1c69718bbcb87f837077c374c34
ppershing <ppershing@3152dc9c-92df-51e6-821f-c0386a2f891b>
2010-04-07 21:58:12 +0000

    Upgradnuty testovac, momentalne zvlada aj submitovat.

--- trunk/testovac/testovac.cpp
+++ trunk/testovac/testovac.cpp
@@ -15,6 +15,10 @@ string Testovac::get_sandbox_dir(){
     return testdir+"/sandbox";
 }
 
+string Testovac::get_tasks_dir(){
+    return testdir+"/tasks";
+}
+
 string Testovac::get_wrapper_dir(){
     return testdir;
 }
@@ -28,26 +32,36 @@ string Testovac::get_program_source(string ext){
 }
 
 void Testovac::clean_sandbox() {
+    // {{{
     assert(initialized);
-    string s="rm -rf \""+get_sandbox_dir()+"/*\"";
-    system(s.c_str());
+    string s="rm -rf "+get_sandbox_dir()+"/*";
+    assert(system(s.c_str())==0);
+    // }}}
 }
 
 void Testovac::copy_wrapper() {
+    // {{{
     assert(initialized);
     string wrapper = get_wrapper_dir()+"/wrapper";
     string sandbox = get_sandbox_dir()+"/wrapper";
     string s="cp \""+wrapper+"\" \""+sandbox+"\"";
-    system(s.c_str());
+    assert(system(s.c_str())==0);
+    // }}}
 }
 
 void Testovac::copy_test_data(const char* taskname) {
-    system((string("cp ") + taskname + "/* sandbox/").c_str());
+    // {{{
+    assert(initialized);
+    string cmd ="cp " + get_tasks_dir()+"/"+taskname+"/* "
+            + get_sandbox_dir();
+    assert(system(cmd.c_str())==0);
+    // }}}
 }
 
 int Testovac::compile(vector<string>& program,
             CompileSettings& settings,
             vector<string> *output) {
+    // {{{
     string programName;
     string compileLine;
     int retval;
@@ -59,7 +73,7 @@ int Testovac::compile(vector<string>& program,
     if (settings.compiler == CompileSettings::COMPILER_CPP) {
         programName = get_program_source("cpp");
         compileLine = "g++ " + programName +
-                      " -static -static-libc" + 
+                      " -static " + 
                       " -o " + get_program_bin();
         if (settings.compile_with_warnings) {
             compileLine+=" -W -Wall";
@@ -84,17 +98,21 @@ int Testovac::compile(vector<string>& program,
 
     read_from_file(error_file.c_str(), output);
     return retval;
+    // }}}
 }
 
 void Testovac::write_to_file(const char * filename, vector<string>& lines) {
+    // {{{
     ofstream s(filename);
     for (int i=0; i<(int) lines.size(); i++) {
         s << lines[i] << endl;
     }
     s.close();
+    // }}}
 }
 
 void Testovac::read_from_file(const char * filename, vector<string> *lines) {
+    // {{{
     ifstream s(filename);
     char buf[1000];
     assert(lines != NULL);
@@ -103,12 +121,15 @@ void Testovac::read_from_file(const char * filename, vector<string> *lines) {
         s.getline(buf, 999);
         lines->push_back(string(buf));
     }
+    // }}}
 }
 
 string Testovac::inttostr(int i){
+    // {{{
     char tmp[100];
     sprintf(tmp,"%d",i);
     return string(tmp);
+    // }}}
 }
 
 int Testovac::test_on_input(vector<string> &program,
@@ -117,6 +138,7 @@ int Testovac::test_on_input(vector<string> &program,
         TestSettings& test_settings,
         vector<string> *compile_output,
         vector<string> *test_output) {
+    // {{{
     int compile_ret;
     compile_ret = Testovac::compile(program, compile_settings, compile_output);
     if (compile_ret) return compile_ret;
@@ -134,15 +156,16 @@ int Testovac::test_on_input(vector<string> &program,
     retval = system(testcmd.c_str());
     read_from_file(test_out.c_str(), test_output);
     return retval;
-
+    // }}}
 }
 
-int Testovac::full_test(const char* taskname,
+int Testovac::submit_solution(const char* taskname,
         vector<string> &program,
         CompileSettings& compile_settings,
         TestSettings& test_settings,
         vector<string> *compile_output,
-        vector<string> *test_output) {
+        vector<string> *test_log) {
+    // {{{
     int compile_ret;
     compile_ret = Testovac::compile(program, compile_settings, compile_output);
     if (compile_ret) return compile_ret;
@@ -150,8 +173,22 @@ int Testovac::full_test(const char* taskname,
     Testovac::copy_test_data(taskname);
 
     int retval;
-    retval = system("cd sandbox && ./testuj.sh 1>test_out 2>test_err");
-    read_from_file("sandbox/test_out", test_output);
+    string wrapper_args = " -m"+inttostr(test_settings.memory_limit);
+    wrapper_args += " -t"+inttostr(test_settings.time_limit);
+    string command = "cd " + get_sandbox_dir() + " && ./test.sh " +
+        wrapper_args + "&> test.log";
+    string logfile = get_sandbox_dir()+"/test.log";
+    retval = system(command.c_str());
+    read_from_file(logfile.c_str(), test_log);
     return retval;
+    // }}}
+}
 
+vector<string> Testovac::get_task_description(const char* taskname) {
+    // {{{
+    vector<string> result;
+    string filename = get_tasks_dir()+"/"+taskname+"/desc";
+    read_from_file(filename.c_str(), &result);
+    return result;
+    // }}}
 }
--- trunk/testovac/testovac.h
+++ trunk/testovac/testovac.h
@@ -18,6 +18,7 @@ public:
 
 class Testovac {
 private:
+    // {{{
     static void clean_sandbox();
     static void copy_wrapper();
     static void copy_test_data(const char* taskname);
@@ -28,15 +29,43 @@ private:
     static string get_sandbox_dir();
     static string get_wrapper_dir();
     static string get_program_bin();
+    static string get_tasks_dir();
     static string get_program_source(string ext);
     static string inttostr(int i);
+    // }}}
 public:
+    /* you must initialize library before any other call,
+       @testdir is path to directory, where Testovac is rooted
+       */
     static void initialize(const char* testdir);
 
+    /* returns task description
+       @taskname - name of task directory, like "sucet", "fibonacci", ...
+       @returns - line by line content od "desc" file within task directory
+       */
+    static vector<string> get_task_description(const char* taskname);
+
+    /* try to compile a program
+        @program - line by line listing of program
+        @compile_settings - set program language, level of compiler
+            warnings
+        @output - line by line output of compiler
+        @returns - 0 on success, nonzero otherwise
+    */
     static int compile(vector<string>& program,
             CompileSettings& settings,
             vector<string> *output);
 
+    /* tests solution of input given by contestant
+        @program - line by line listing of program
+        @input - line by line listing of input on which program
+            should be tested
+        @compile_settings - see submit_solution()
+        @test_setting - see submit_solution()
+        @compile_output - see submit_solution()
+        @test_output - line by line output of program running on input data
+        @returns - 0 on succes, nonzero value otherwise
+        */
     static int test_on_input(vector<string>& program,
             vector<string>& input,
             CompileSettings& compile_settings,
@@ -44,10 +73,20 @@ public:
             vector<string> *compile_output,
             vector<string> *test_output);
 
-    static int full_test(const char* taskname,
+    /* tests solution on all prepared input/output data
+        @taskname - name of task directory, like "sucet", "fibonacci", ...
+        @program - line by line listing of program to be submitted
+        @compile_settings - set program compile settings (language, warning
+            levels)
+        @test_settings - set memory and time limit
+        @compile_output - will be filled with line by line output of compiler
+        @test_log - will be filled with line by line output of tester
+        @returns - 0 on success, nonzero value otherwise
+        */
+    static int submit_solution(const char* taskname,
             vector<string>& program,
             CompileSettings& compile_settings,
             TestSettings& test_settings,
             vector<string> *compile_output,
-            vector<string> *test_output);
+            vector<string> *test_log);
 };
