eabed2b0d2d2584fd7084b660d86b4754a67ebe6
ppershing <ppershing@3152dc9c-92df-51e6-821f-c0386a2f891b>
2010-04-10 12:36:47 +0000

    Farebny test.sh, este treba spravit aby to spolupracovalo s QTckom

--- testovac/task_data/test.sh
+++ testovac/task_data/test.sh
@@ -1,19 +1,23 @@
-echo "Test parameters: $*"
+echo "<font color='yellow'> Welcome to testing session"
+echo "Test parameters I'm using now are '$*' </font>"
 
 for infile in *.in; do
     echo ""
-    echo "********* Testing $infile ********"
+    echo "<font color='yellow'> ********* Testing $infile ******** </font>"
     base=`basename $infile .in`
     outfile=$base.out
     ./wrapper $* ./program -i$infile -o$base.tst
     if [ "$?" -ne 0 ]; then
-        echo ">>>> Program ended unexpectedly at case $base"
+        echo -n "<font color='red'> ****** Program ended unexpectedly "
+        echo "at input $base </font>"
         exit 1
     fi;
     diff $base.tst $base.out;
     if [ "$?" -ne 0 ]; then
-        echo ">>>> The output of case $base is not correct"
+        echo -n "<font color='red'> **** The output of testcase "
+        echo " $base is not correct </font>"
         exit 1
     fi;
 done
+echo "<font color='green'> Everything seems to be OK, accepting </font>"
 exit 0;
