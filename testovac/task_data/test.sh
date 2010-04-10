90e53d88f6397ada076863b72b75dff8490a3187
ppershing <ppershing@3152dc9c-92df-51e6-821f-c0386a2f891b>
2010-04-10 17:29:41 +0000

    Upraveny default test skript na lepsie formatovanie.

--- testovac/task_data/test.sh
+++ testovac/task_data/test.sh
@@ -1,31 +1,39 @@
-echo "<font color='yellow'> Welcome to testing session"
+echo "<font color='yellow'> <h2> Welcome to testing session </h2> </font>"
 
 SHOW_DIFF=0
 if [ "$1" == "--fulllog" ]; then
     SHOW_DIFF=1
     shift
 fi
-echo "Test parameters I'm using now are '$*', full_log: $SHOW_DIFF </font>"
+echo "<p><font color='yellow'> Test parameters I'm using now are: '$*', "
+echo "full_log_enabled: $SHOW_DIFF </font></p>"
 
 for infile in *.in; do
-    echo ""
-    echo "<font color='yellow'> ********* Testing $infile ******** </font>"
+    echo "<p><font color='yellow'> ********* "
+    echo "Testing $infile ******** </font><br>"
+
     base=`basename $infile .in`
     outfile=$base.out
+    echo -n "<font color='yellow'> running... </font> <pre>"
     ./wrapper $* ./program -i$infile -o$base.tst
+    echo "</pre>"
+
     if [ "$?" -ne 0 ]; then
         echo -n "<font color='red'> ****** Program ended unexpectedly "
         echo "at input $base </font>"
         exit 1
     fi;
     echo "<font color='yellow'> program exitted normally"
-    echo "checking output for validity...</font>"
+    echo "checking output for validity...</font><br>"
 
     if [ $SHOW_DIFF -ne 0 ]; then
         diff $base.tst $base.out > $base.diff;
         DIFFRES=$?
-        cat $base.diff | sed 's/^</ <font color="red">mine:<\/font>/' \
-                | sed 's/^>/ <font color="orange">correct:<\/font>/';
+        echo "<font color='yellow'> Diff: </font><pre> "
+        cat $base.diff | sed 's/</≪/' \
+                | sed 's/>/≫/' \
+                | sed 's/&/∝/';
+        echo "</pre>";
     else
         diff $base.tst $base.out > /dev/null
         DIFFRES=$?
@@ -35,6 +43,8 @@ for infile in *.in; do
         echo " $base is not correct </font>"
         exit 1
     fi;
+    echo "<font color='green'> OK, empty diff </font>"
+    echo "</p>"
 done
 echo "<font color='green'> Everything seems to be OK, accepting </font>"
 exit 0;
