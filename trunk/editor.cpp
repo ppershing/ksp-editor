59290072935d572e16f97c20db0beea8f7891d93
mirelon <mirelon@3152dc9c-92df-51e6-821f-c0386a2f891b>
2010-04-13 00:07:57 +0000

    delayed keys upgrade

--- trunk/editor.cpp
+++ trunk/editor.cpp
@@ -10,8 +10,24 @@ void Editor::keyPressEvent(QKeyEvent *e){
 	emit keystroke();
 	if(e->key()==Qt::Key_Backspace && !s->getBool("upgrades/backspace"))return;
 	if((e->key()==Qt::Key_PageUp || e->key()==Qt::Key_PageDown) && !s->getBool("upgrades/pageUpDown"))return;
-	QTextEdit::keyPressEvent(e);
+	if(e->text().length()==0)
+		QTextEdit::keyPressEvent(e);
+	else{
+
+		//napad: mozem zahadzovat ked je vela klaves naraz
+
+		klavesy.enqueue(e->text());
+		QTimer::singleShot(3000 - 1500*s->getInt("upgrades/delay1")-1000*s->getInt("upgrades/delay2")-500*s->getInt("upgrades/delay3"),this,SLOT(delayedKey()));
+	}
 	lastEditTime = QTime::currentTime();
+}
+
+void Editor::delayedKey(){
+	qDebug() << klavesy.length();
+	if(klavesy.isEmpty())return;
+	qDebug() << "not empty";
+	qDebug() << klavesy.head();
+	insertPlainText(klavesy.dequeue());
 }
 
 void Editor::mousePressEvent(QMouseEvent *e){
