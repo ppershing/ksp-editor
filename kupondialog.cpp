8d081deb499106d721cd8bc6f819f986a2775702
mirelon <mirelon@3152dc9c-92df-51e6-821f-c0386a2f891b>
2010-04-14 16:30:47 +0000

    settings are written to custom user file custom.ini (delete it to reset), valid codes are in codes.txt

--- kupondialog.cpp
+++ kupondialog.cpp
@@ -26,8 +26,40 @@ void KuponDialog::changeEvent(QEvent *e)
     }
 }
 
+void KuponDialog::loadCodes(){
+	QFile* f = new QFile("codes.txt");
+	f->open(QIODevice::ReadOnly);
+	while(!f->atEnd()){
+		codes.insert(QString::fromAscii(f->readLine()).trimmed());
+	}
+	f->close();
+}
+
+void KuponDialog::clearCodes(){
+	codes.clear();
+}
+
+int KuponDialog::kreditGain(QString code){
+	return code.length()*code.length()*code.length();
+}
+
 void KuponDialog::accept(){
-	if(ui->lineEdit->text()=="tazb")
-		s->increment("credits/count",1);
+	QString code = ui->lineEdit->text();
+	clearCodes();
+	loadCodes();
+
+	if(!codes.contains(code)){
+		qDebug() << "wrong code";
+		QDialog::reject();
+		return;
+	}
+	if(s->isset("used/"+code)){
+		qDebug() << "already used code";
+		QDialog::reject();
+		return;
+	}
+
+	s->setProp("used/"+code,1);
+	s->increment("credits/count",kreditGain(code));
 	QDialog::accept();
 }
